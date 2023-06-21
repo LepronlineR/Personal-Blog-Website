09e4a5297ed2b3b649a2b7a357ef5ad40d4ccc09
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2023-06-21 00:35:25 -0400

    redoing navbar

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import MenuItems from './MenuItems'
 import MenuItemsNewTab from './MenuItemsNewTab'
-import SearchBar from './SearchBar'
+import { faLinkedin, faTwitch, faYoutube, faGithub, faItchIo } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHomeLg } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
     <div 
-        className="h-[50px] flex shadow-xl sticky top-0"
+        className="h-[100px] flex top-0"
     >
         <div
             className="flex items-center w-full mx-12"
         >
-            <MenuItems 
-                pageName="Home"
-                pageID=""
+            <MenuItems
+                pageName="Blog"
+                pageID="articles"
             />
             <MenuItems 
                 pageName="About Me"
@@ -24,9 +26,50 @@ const Navbar = () => {
                 pageName="Portfolio"           
             />
         </div>
-        <SearchBar
-            className="flex items-center w-full"
-        />
+        <div
+            className="flex items-center w-full ml-80"
+        >
+            <MenuItems 
+                pageName = {
+                    <FontAwesomeIcon icon={faHomeLg} className="scale-[2]"/>
+                }
+                pageID = ""
+            />
+        </div>
+        <div
+            className="flex items-center mx-12"
+        >
+            <MenuItemsNewTab 
+                websiteName="https://www.linkedin.com/in/zhizheng1/"
+                pageName = {
+                    <FontAwesomeIcon icon={faLinkedin} className="scale-150 mr-16"/>
+                }
+            />
+            <MenuItemsNewTab 
+                websiteName="https://www.twitch.tv/fzsav"
+                pageName = {
+                    <FontAwesomeIcon icon={faTwitch} className="scale-150 mr-16"/>
+                }
+            />
+            <MenuItemsNewTab 
+                websiteName="https://www.youtube.com/channel/UCd3Yj4spgtDW5O1WV1HgNag"
+                pageName = {
+                    <FontAwesomeIcon icon={faYoutube} className="scale-150 mr-16"/>
+                }
+            />
+            <MenuItemsNewTab 
+                websiteName="https://github.com/LepronlineR"
+                pageName = {
+                    <FontAwesomeIcon icon={faGithub} className="scale-150 mr-16"/>
+                }
+            />
+            <MenuItemsNewTab 
+                websiteName="https://fzsav.itch.io"
+                pageName = {
+                    <FontAwesomeIcon icon={faItchIo} className="scale-150 mr-16"/>
+                }
+            />
+        </div>
     </div>
   )
 }
