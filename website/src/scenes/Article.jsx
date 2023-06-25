58cd95b005e34bc68163f694c79d65250363c6c9
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2023-06-25 19:13:35 -0400

    save

--- website/src/scenes/Article.jsx
+++ website/src/scenes/Article.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 import image from '../assets/images/fibonacci-naive.png'
-//import latex from '../assets/latex/algorithms.tex'
+import latex from '../assets/latex/algorithms.tex'
 
 const Article = () => {
+
+  const config = {
+    loader: { load: ["[tex]/html"]},
+    tex: {
+      packages: { "[+]": ["html"]},
+      inlineMath: [["$", "$"]],
+      displayMath: [["$$", "$$"]]
+    }
+  }
 
   const equation = "$x+y^2 = z$";
 
   return (
-    <MathJaxContext>
+    <MathJaxContext version={3} config={config}>
           <h2>Basic MathJax example with Latex</h2>
           <MathJax>{equation}</MathJax>
     </MathJaxContext>
